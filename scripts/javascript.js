4f29b1bd4c3a026a3e029a104a9572bfa4967906
Yukta Sagar <sagaryukta@gmail.com>
2015-10-07 16:09:44 -0400

    object constructor
    doesn't seem to be working out

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,8 +1,6 @@
 (function(){
 	var hamburger = document.getElementById('navIcon');
 	var menu = document.getElementById('menu');
-	alert("I am an alert box!");
-	var myString = "this is a string";
 hamburger.addEventListener("click", function(){
 	if(menu.classList.contains('shown')) {
 		menu.classList.remove('shown')
@@ -10,4 +8,12 @@ hamburger.addEventListener("click", function(){
 		menu.classList.add('shown')
 	}
 });
+	function product(price, size, color) {
+		this.price = price;
+		this.size = size;
+		this.color = color;
+	}
+	var mugs = new product("$20", "4x5", "blue");
+	var bowls = new product("$30", "Rally", "green"); 
+
 })();
